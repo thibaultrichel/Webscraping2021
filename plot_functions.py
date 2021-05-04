abc70dc9a76645c032a494e8778b0e6085ce7ef5
PacPacaud <victor-alexis.pacaud@esme.fr>
2021-05-04 15:05:30 +0200

    blc

--- plot_functions.py
+++
@@ -1,9 +0,0 @@
-﻿import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv('dftest.csv', sep=';')
-
-nbCases = df['NbCases']
-nbDeaths = df['NbDeaths']
-barrelPrice = df['BarrelPrice']
