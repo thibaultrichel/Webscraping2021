43f1612e8f25ad50e5ec17f036d8ec77e9bf09f4
PacPacaud <victor-alexis.pacaud@esme.fr>
2021-05-05 14:41:59 +0200

    S_p_modif_dateOK

--- show_plots.py
+++ show_plots.py
@@ -12,23 +12,19 @@ dates = df['Date'].iloc[::-1]
 nbCases = df['NbCases'].iloc[::-1]
 nbDeaths = df['NbDeaths'].iloc[::-1]
 barrelPrice = df['BarrelPrice'].iloc[::-1]
-newsTitles = df['news Titles'].iloc[::-1]
+newsTitles = df['News Titles'].iloc[::-1]
 
 
 
-#plt.title('prix baril')
 
 # graph nb de cas
 ax1 = plt.subplot(3, 1, 1)
 plt.bar(dates, nbCases, label='Nombre de cas de COVID-19')
-plt.ylim(4000000)
+
 plt.ylabel('nombre de cas')
 plt.setp(ax1.get_xticklabels(), visible=False)
+plt.xticks(np.arange(0, len(dates), 20), rotation='45')
 plt.legend()
-#plt.title('nb de cas de COVID-19')
-#plt.show()
-
-
 
 # graph nb de décès
 ax2 = plt.subplot(3, 1, 2)
@@ -36,19 +32,18 @@ plt.plot(dates, nbDeaths, label='Nombre de décès', )
 
 plt.ylabel('nombre de décès')
 plt.setp(ax2.get_xticklabels(), visible=False)
+plt.xticks(np.arange(0, len(dates), 20), rotation='45')
 plt.legend()
-#plt.title('nb de decès')
-#plt.show()
 
 # graph prix du baril
 ax3 = plt.subplot(3, 1, 3)
 plt.plot(dates, barrelPrice, label='Prix du baril')
 plt.xlabel('Dates')
 plt.ylabel('prix baril en $')
-plt.xticks(dates, rotation='vertical')
+plt.xticks(np.arange(0, len(dates), 20), rotation='45')
 plt.setp(ax3.get_xticklabels(), visible=True)
 plt.legend()
 
 
-mplcursors.cursor(hover=True)
+
 plt.show()
