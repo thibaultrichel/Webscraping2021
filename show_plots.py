3d5aa0c91d5bd35b7385ca4eed75d5f72b6e806b
thibzzz09 <thibault.richel2@gmail.com>
2021-05-07 15:25:16 +0200

    modif plot

--- show_plots.py
+++ show_plots.py
@@ -1,8 +1,9 @@
+import matplotlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
-import mplcursors
+
 
 figure(figsize=(8, 10), dpi=80)
 
@@ -14,36 +15,34 @@ nbDeaths = df['NbDeaths'].iloc[::-1]
 barrelPrice = df['BarrelPrice'].iloc[::-1]
 newsTitles = df['News Titles'].iloc[::-1]
 
-
-
-
 # graph nb de cas
 ax1 = plt.subplot(3, 1, 1)
-plt.bar(dates, nbCases, label='Nombre de cas de COVID-19')
-
+ax1.grid(zorder=0, linestyle='--')
+plt.bar(dates, nbCases, label='Nombre de cas de COVID-19', zorder=3)
+ax1.get_yaxis().set_major_formatter(
+    matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 plt.ylabel('nombre de cas')
 plt.setp(ax1.get_xticklabels(), visible=False)
-plt.xticks(np.arange(0, len(dates), 20), rotation='45')
+plt.xticks(np.arange(0, len(dates), 20), rotation='vertical')
 plt.legend()
 
 # graph nb de décès
 ax2 = plt.subplot(3, 1, 2)
 plt.plot(dates, nbDeaths, label='Nombre de décès', )
-
+plt.grid(linestyle='--')
 plt.ylabel('nombre de décès')
 plt.setp(ax2.get_xticklabels(), visible=False)
-plt.xticks(np.arange(0, len(dates), 20), rotation='45')
+plt.xticks(np.arange(0, len(dates), 20), rotation='vertical')
 plt.legend()
 
 # graph prix du baril
 ax3 = plt.subplot(3, 1, 3)
 plt.plot(dates, barrelPrice, label='Prix du baril')
+plt.grid(linestyle='--')
 plt.xlabel('Dates')
 plt.ylabel('prix baril en $')
-plt.xticks(np.arange(0, len(dates), 20), rotation='45')
+plt.xticks(np.arange(0, len(dates), 20), rotation='vertical')
 plt.setp(ax3.get_xticklabels(), visible=True)
 plt.legend()
-
-
 
 plt.show()
